dbfe09ce56272681990610d560e4b7a819fe3992
Viktor <vsybrex@gmail.com>
2020-02-25 19:09:40 +0200

    save progress

--- bot.py
+++ bot.py
@@ -17,16 +17,22 @@ logger = logging.getLogger(__name__)
 # Main states
 SELECT_TOPIC, SEARCH_TOPIC, ADD_TOPIC_INTRO, ADD_TOPIC, EDIT_TOPIC = range(5)
 
+# Stories states
+SELECT_STORY_TYPE, EDIT_STORY, VIDEO_STORY, PHOTO_STORY, TEXT_STORY = range(6, 11)
+
+# Constants
+TOPIC_START_OVER, START_OVER = range(11, 13)
+
 # Meta states
-STOPPING, SHOWING = map(chr, range(8, 10))
+STOPPING, SHOWING = range(13, 15)
 
 # Shortcut for ConversationHandler.END
 END = ConversationHandler.END
 
 
 def start(update, context):
-    user = update.message.from_user
-    logger.info("User %s started the conversation.", user.first_name)
+    logger.info('Starting')
+    logger.info('Update: %s', update)
 
     keyboard = [
         [InlineKeyboardButton(text='Winter', callback_data='winter'),
@@ -38,42 +44,22 @@ def start(update, context):
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
 
-    update.message.reply_text(
-        'Greetings',
-        reply_markup=reply_markup
-    )
+    if context.user_data.get(START_OVER):
+        text = 'Here we go again!'
+        update.callback_query.edit_message_text(text=text, reply_markup=reply_markup)
+    else:
+        text = 'Greetings'
+        update.message.reply_text(text=text, reply_markup=reply_markup)
 
-    return SELECT_TOPIC
+    context.user_data[START_OVER] = False
 
-
-def start_over(update, context):
-    query = update.callback_query
-
-    bot = context.bot
-    keyboard = [
-        [InlineKeyboardButton(text='Winter', callback_data='winter'),
-         InlineKeyboardButton(text='Spring', callback_data='spring'),
-         InlineKeyboardButton(text='Summer', callback_data='summer'),
-         InlineKeyboardButton(text='Autumn', callback_data='autumn'),
-         InlineKeyboardButton(text='Search', callback_data='search'),
-         InlineKeyboardButton(text='New', callback_data='new')]
-    ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-
-    bot.edit_message_text(
-        chat_id=query.message.chat_id,
-        message_id=query.message.message_id,
-        text='Here we go again',
-        reply_markup=reply_markup
-    )
     return SELECT_TOPIC
 
 
 def edit_topic(update, context):
     logger.info('Editing topic')
-    query = update.callback_query
+    logger.info('Update: %s', update)
 
-    bot = context.bot
     keyboard = [
         [InlineKeyboardButton(text='Video', callback_data='video'),
          InlineKeyboardButton(text='Photo', callback_data='photo'),
@@ -83,18 +69,21 @@ def edit_topic(update, context):
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
 
-    bot.edit_message_text(
-        chat_id=query.message.chat_id,
-        message_id=query.message.message_id,
-        text='Editing topic',
-        reply_markup=reply_markup
-    )
-    return EDIT_TOPIC
+    if context.user_data.get(TOPIC_START_OVER):
+        text = 'Got it! Story name. Please select a story type'
+        update.message.reply_text(text=text, reply_markup=reply_markup)
+    else:
+        text = 'Story name. Please select a story type'
+        update.callback_query.edit_message_text(text=text, reply_markup=reply_markup)
+
+    context.user_data[TOPIC_START_OVER] = False
+
+    return SELECT_STORY_TYPE
 
 
 def search_topic_intro(update, context):
     logger.info('Search topic')
-    text = 'Okay, type the topic name'
+    text = 'Okay, type the topic name to search'
     update.callback_query.edit_message_text(text=text)
     return SEARCH_TOPIC
 
@@ -106,29 +95,16 @@ def search_topic(update, context):
 
 def new_topic(update, context):
     logger.info('New topic')
-    text = 'Okay, type the topic name'
+    text = 'Okay, type the new topic name'
     update.callback_query.edit_message_text(text=text)
     return ADD_TOPIC
 
 
-def video_story(update, context):
-    logger.info('Video story')
-    return SELECT_TOPIC
-
-
-def photo_story(update, context):
-    logger.info('Photo story')
-    return SELECT_TOPIC
-
-
-def text_story(update, context):
-    logger.info('Text story')
-    return SELECT_TOPIC
-
-
-def exit_nested(update, context):
-    update.message.reply_text('Okay, bye.')
-    return STOPPING
+def close_topic(update, context):
+    logger.info('Closing topic')
+    context.user_data[START_OVER] = True
+    start(update, context)
+    return END
 
 
 def save_topic(update, context):
@@ -142,40 +118,83 @@ def topic_saved(update, context):
     return END
 
 
+def edit_topic_story(update, context):
+    logger.info('Editing topic story')
+    return EDIT_STORY
+
+
+def video_story(update, context):
+    logger.info('Video story')
+    return END
+
+
+def photo_story(update, context):
+    logger.info('Photo story')
+    return END
+
+
+def text_story(update, context):
+    logger.info('Saving text story: %s', update.message.text)
+    context.user_data[TOPIC_START_OVER] = True
+    return edit_topic(update, context)
+
+
+def ask_for_story(update, context):
+    logger.info('Asking for story')
+    logger.info('Update: %s', update)
+    if update.callback_query.data == 'text':
+        text = 'Input text of the story'
+        update.callback_query.edit_message_text(text=text)
+        return TEXT_STORY
+    elif update.callback_query.data == 'video':
+        return VIDEO_STORY
+    elif update.callback_query.data == 'photo':
+        return PHOTO_STORY
+
+
+def search_story(update, context):
+    logger.info('Search for story')
+    return EDIT_STORY
+
+
+def close_nested(update, context):
+    update.message.reply_text('Okay, bye.')
+    return STOPPING
+
+
 def end(update, context):
     logger.info('Exit')
-    query = update.callback_query
-    bot = context.bot
-    bot.edit_message_text(
-        chat_id=query.message.chat_id,
-        message_id=query.message.message_id,
-        text='See you next time'
-    )
-    return ConversationHandler.END
+    return END
 
 
 def error(update, context):
-    logger.info('Update "%s" caused error "%s"', update, context.error)
+    logger.error('Update "%s" caused error "%s"', update, context.error)
 
 
 def main():
     updater = Updater(env['telegram']['token'], use_context=True)
     dp = updater.dispatcher
 
-    # Adding new topic conversation
-    new_topic_conv = ConversationHandler(
-        entry_points=[CallbackQueryHandler(new_topic, pattern='^new$')],
+    # Edit topic stories conversation
+    topic_conv = ConversationHandler(
+        entry_points=[CallbackQueryHandler(edit_topic, pattern='^winter|spring|summer|autumn$')],
         states={
-            ADD_TOPIC_INTRO: [CallbackQueryHandler(new_topic, pattern='^(?!' + str(END) + ').*$')],
-            ADD_TOPIC: [MessageHandler(Filters.text, save_topic)],
+            SELECT_STORY_TYPE: [
+                CallbackQueryHandler(ask_for_story, pattern='^photo|text|video$'),
+                CallbackQueryHandler(edit_topic_story, pattern='^edit$'),
+                CallbackQueryHandler(close_topic, pattern='^back$')
+            ],
+            EDIT_STORY: [MessageHandler(Filters.text, search_story)],
+            VIDEO_STORY: [MessageHandler(Filters.video, video_story)],
+            PHOTO_STORY: [MessageHandler(Filters.photo, photo_story)],
+            TEXT_STORY: [MessageHandler(Filters.text, text_story)]
         },
         fallbacks=[
-            CallbackQueryHandler(topic_saved, pattern='^' + str(END) + '$'),
-            CommandHandler('exit', exit_nested)
+            CommandHandler('exit', close_nested)
         ],
         map_to_parent={
-            END: SELECT_TOPIC,
             STOPPING: STOPPING,
+            END: SELECT_TOPIC
         }
     )
 
@@ -184,18 +203,12 @@ def main():
         entry_points=[CommandHandler('start', start)],
         states={
             SELECT_TOPIC: [
-                CallbackQueryHandler(edit_topic, pattern='^winter|spring|summer|autumn$'),
+                topic_conv,
                 CallbackQueryHandler(search_topic_intro, pattern='^search$'),
-                new_topic_conv
+                CallbackQueryHandler(new_topic, pattern='^new$')
             ],
             SEARCH_TOPIC: [
                 MessageHandler(Filters.text, search_topic)
-            ],
-            EDIT_TOPIC: [
-                CallbackQueryHandler(video_story, pattern='^video'),
-                CallbackQueryHandler(photo_story, pattern='^photo'),
-                CallbackQueryHandler(text_story, pattern='^text'),
-                CallbackQueryHandler(start_over, pattern='^back'),
             ]
         },
         fallbacks=[CommandHandler('exit', end)]
