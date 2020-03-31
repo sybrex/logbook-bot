c1ad2d43280fb54632ee1533205486870b858d51
Viktor <vsybrex@gmail.com>
2020-03-31 17:37:40 +0300

    deployment updates

--- fabfile.py
+++ fabfile.py
@@ -3,8 +3,8 @@ from fabric.tasks import task
 
 SYSTEMD_SERVICE = 'logbook-bot'
 USERNAME = 'deployer'
-TARGET_DIRECTORY = '/srv/www/logbook-bot'
-GIT_REPOSITORY = 'git@github.com:sybrex/logbook-bot.git'
+PROJECT_PATH = '/srv/www/logbook-bot'
+GIT_REPOSITORY = 'logbook-bot.github.com:sybrex/logbook-bot.git'
 GIT_KEY = '~/.ssh/github-deployer'
 
 
@@ -14,8 +14,8 @@ def install(c):
     Install project
     pipenv run fab install --hosts ip
     """
-    c.run(f'mkdir {TARGET_DIRECTORY}')
-    with c.cd(TARGET_DIRECTORY):
+    c.run(f'mkdir {PROJECT_PATH}')
+    with c.cd(PROJECT_PATH):
         c.run(f'git clone {GIT_REPOSITORY} .')
         c.run('export PIPENV_VENV_IN_PROJECT="enabled" && pipenv install')
 
@@ -26,7 +26,7 @@ def upload(c, local, remote):
     Upload file
     pipenv run fab upload --local /path/to/local/file.txt --remote relative/path/to/file.txt --hosts ip
     """
-    c.put(local, f'{TARGET_DIRECTORY}/{remote}')
+    c.put(local, f'{PROJECT_PATH}/{remote}')
 
 
 @task
@@ -35,7 +35,7 @@ def download(c, remote, local):
     Download file
     pipenv run fab download --remote relative/path/to/file.txt --local /path/to/local/file.ini --hosts ip
     """
-    c.get(f'{TARGET_DIRECTORY}/{remote}', local)
+    c.get(f'{PROJECT_PATH}/{remote}', local)
 
 
 @task
@@ -44,10 +44,20 @@ def deploy(c, branch='master', deps=True):
     Deploy updates to server
     pipenv run fab deploy --branch master --deps --hosts ip
     """
-    with c.cd(TARGET_DIRECTORY):
+    with c.cd(PROJECT_PATH):
         print('Update code')
         c.run(f'git fetch origin && git checkout {branch} && git pull origin {branch}')
         if deps:
             print('Installing dependencies')
             c.run('pipenv install')
-        c.run(f'sudo service {SYSTEMD_SERVICE} restart')
+            service(c, SYSTEMD_SERVICE, 'restart')
+
+
+@task
+def service(c, name="nginx", action="restart"):
+    """
+    System service status|start|stop|restart
+    pipenv run fab service --name nginx --action stop --hosts ip
+    """
+    c.run(f'sudo service {name} {action}')
+
